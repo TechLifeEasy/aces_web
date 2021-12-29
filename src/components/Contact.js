c005f1f7b866f8cbeba11551e410abdb81795fcf
Dhruvil Shah <dhruvilshah2001@gmail.com>
2021-12-29 16:22:54 +0530

    added image with styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import {BsFacebook,BsInstagram,BsLinkedin} from 'react-icons/bs'
 
 export const Contact = () => {
     return (
         <>
-<div className=" flex p-10 w-1/2 h-1/2 mx-auto justify-center flex-col ">
-  <form className="p-10 bg-slate-800 rounded-2xl shadow-2xl shadow-black-10/40">
+        <div className='flex flex-row'>
+  <div className='bg-black my-auto'>
+  <img src={process.env.PUBLIC_URL + '/image/contact.jpg'} /> 
+  </div> 
+  <div className=" flex p-2 w-1/2 bg-no-repeat h-1/2 mx-auto justify-center flex-col ">
+  <form className="p-10  bg-slate-800 rounded-2xl shadow-2xl shadow-black-10/40">
   <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
       <label className="block uppercase tracking-wide border-gray-200 text-slate-400 text-xs font-bold mb-2" for="grid-first-name">
         First Name
       </label>
@@ -50,14 +55,21 @@ export const Contact = () => {
       <button className="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded" type="submit">
         Send Message
       </button>
-    </div>
       
+    </div>
+    <div className='flex flex-row p-5 gap-7 justify-center items-center  m-auto mt-2'>
+    <a href='https://www.facebook.com/aces.itnu/' target='_blank'><BsFacebook className='cursor-pointer'  size={20}  color='white'/></a>
+    <a href='https://www.instagram.com/aces_it_nu/' target='_blank'><BsInstagram  className='cursor-pointer' size={20} color='white'/></a>
+    <a href='https://www.linkedin.com/company/association-of-computer-engineering-students/mycompany/' target='_blank'><BsLinkedin  className='cursor-pointer' size={20} color='white'/></a>
+    </div>
   </div>
-    
 </form>
-  
 </div>
 
+
+
+
+</div>
 </>
 
 
