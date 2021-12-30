d756bf83e3e93a59ac1bd9c09118f253e70ae1c6
Barun Debnath <barundebnath91@gmail.com>
2021-12-30 17:20:19 +0530

    Components added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import Socials from "./components/Socials";
 
-
-
 // import Events from "./components/Events";
 // import Team from './components/Team';
-import {Navbar} from './components/Navbar'
-import { useState ,useEffect} from "react";
-import Footer from './components/Footer';
-// import Navbar2  from "./components/Navbar2";
+import { Navbar } from "./components/Navbar";
+import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import Navbar2 from "./components/Navbar2";
+import HeroContent from "./components/HeroContent";
+import CTA from "./components/CTA";
+import DiscordLink from "./components/DiscordLink";
+import EventCard from "./components/EventCard";
 
-import {BsFillSunFill,BsMoonFill} from 'react-icons/bs';
+import { BsFillSunFill, BsMoonFill } from "react-icons/bs";
 
 function App() {
+  const [isLightTheam, setLightTeam] = useState(true);
 
-  const [isLightTheam,setLightTeam]=useState(true);
-
-  useEffect(()=>{
-    if(isLightTheam){
-      document.querySelector('body').classList.remove('dark')
-      document.querySelector('body').classList.remove('body_d')
-    }else{
-      document.querySelector('body').classList.add('dark')
-      document.querySelector('body').classList.add('body_d')
+  useEffect(() => {
+    if (isLightTheam) {
+      document.querySelector("body").classList.remove("dark");
+      document.querySelector("body").classList.remove("body_d");
+    } else {
+      document.querySelector("body").classList.add("dark");
+      document.querySelector("body").classList.add("body_d");
     }
-  },[isLightTheam])
+  }, [isLightTheam]);
 
   return (
-
-
     <div className={`flex flex-col`}>
-     
-     <Navbar/>
-     {/* <Navbar2/> */}
-     <Socials></Socials>
+      {/* <Navbar /> */}
+      <Navbar2 />
+      <HeroContent />
+      <CTA />
+      <EventCard />
+      <DiscordLink />
+      <Socials></Socials>
 
-
-     <div className=" absolute right-10 top-1 text-white cursor-pointer"  onClick={()=>setLightTeam(!isLightTheam)}>
+      {/* <div className=" absolute right-10 top-1 text-white cursor-pointer"  onClick={()=>setLightTeam(!isLightTheam)}>
        
 
        {
@@ -47,14 +48,10 @@ function App() {
        }
 
        
-     </div>
+     </div> */}
 
-     <Footer></Footer>
-    
-
+      <Footer></Footer>
     </div>
-
-  
   );
 }
 
